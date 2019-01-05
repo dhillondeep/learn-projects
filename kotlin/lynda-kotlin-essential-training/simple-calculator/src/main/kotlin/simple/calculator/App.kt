b0b97550d63ca4dd402d5636585ce4698de94669
Deep Dhillon <deep@deepdhillon.ca>
2019-01-05 12:06:48 -0500

    simple calculator finished

--- kotlin/lynda-kotlin-essential-training/simple-calculator/src/main/kotlin/simple/calculator/App.kt
+++ kotlin/lynda-kotlin-essential-training/simple-calculator/src/main/kotlin/simple/calculator/App.kt
@@ -1,5 +1,28 @@
 package simple.calculator
 
 fun main(args: Array<String>) {
-    println("Hello world calculator")
+    try {
+        print("Number 1: ")
+        val number1 = readLine()!!.toDouble()
+
+        print("Number 2: ")
+        val number2 = readLine()!!.toDouble()
+
+        print("Select an operation (+ - / *): ")
+        val operation = readLine()
+
+        val result: Double = when (operation) {
+            "+" -> number1 + number2
+            "-" -> number1 - number2
+            "/" -> number1 / number2
+            "*" -> number1 * number2
+            else -> throw java.lang.Exception("Unknown operator")
+        }
+
+        println("The answer is $result")
+    } catch (e: NumberFormatException) {
+        println("${e.message} is not a number")
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
